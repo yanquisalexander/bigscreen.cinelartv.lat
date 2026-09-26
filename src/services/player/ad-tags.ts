c4c07b755488ae199294c70475b0afd515956a29
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2026-09-26 11:40:08 -0300

    .

--- src/services/player/ad-tags.ts
+++ src/services/player/ad-tags.ts
@@ -10,6 +10,10 @@ const PREROLL_TAGS = [
     label: 'adp3-rtb',
   },
   {
+    url: 'https://expensive-pollution.com/damcF.zhdxGRNjvHZpGkUU/-ermf9EuUZ/UqljkuPIT/cSx/OLD/IZwbNGDiUUtzNkzsEj4yMOjSAP0QO/SvZFsmaKWg1RpAdiDh0cxd',
+    label: 'hilltopads',
+  },
+  {
     url: 'https://pubads.g.doubleclick.net/gampad/live/ads?iu=/22530741549/CTV_VAST_ADS&description_url=[DESCRIPTION_URL]&tfcd=0&npa=0&sz=400x300%7C640x480&gdfp_req=1&unviewed_position_start=1&output=vast&env=vp&impl=s&correlator=[CACHEBUSTER]',
     label: 'google-ad-manager',
   },
@@ -17,10 +21,7 @@ const PREROLL_TAGS = [
      url: 'https://s.magsrv.com/v1/vast.php?idz=5963002',
      label: 'exoclick',
    }, */
-  {
-    url: 'https://expensive-pollution.com/damcF.zhdxGRNjvHZpGkUU/-ermf9EuUZ/UqljkuPIT/cSx/OLD/IZwbNGDiUUtzNkzsEj4yMOjSAP0QO/SvZFsmaKWg1RpAdiDh0cxd',
-    label: 'hilltopads',
-  },
+
 ];
 
 const POSTROLL_TAGS = [
